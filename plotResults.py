381b9d614db735fa7ed98ca57f94b9ba90548331
Guillermocidm <39138459+Guillermocidm@users.noreply.github.com>
2021-05-02 10:19:00 +0200

    res

--- plotResults.py
+++ plotResults.py
@@ -4,16 +4,16 @@ import os
 import ast
 import matplotlib.pyplot as plt
 
-def plot_res(list_of_res,mu_values):
+def plot_res(list_of_res,mu_values,std):
     fig, ax = plt.subplots(1)
-    for res,mu,graph in zip(list_of_res,mu_values,graphs):
+    for res,mu,s in zip(list_of_res,mu_values,std):
         ax.plot(beta_values,res,label = 'mu = '+mu)
         ax.legend()
         ax.set_title(graph_path.split('/')[-1][:-4])
         ax.legend(loc='lower right')
         ax.set_xlabel(r'$\beta$')
         ax.set_ylabel(r'$\rho$')
-        #ax.fill_between(beta_values, res - std , res + std, alpha=0.3)
+        ax.fill_between(beta_values, np.array(res) - np.array(s) , np.array(res) + np.array(s), alpha=0.7)
     fig.show()
     fig.savefig('results_'+graph_path.split('/')[-1][:-4]+'.png',dpi=100)
 
@@ -27,12 +27,20 @@ for graph_path in graphs:
     net = nx.Graph(nx.read_pajek(graph_path))
     graph_name = graph_path.split('/')[-1][:-4]
     list_of_res = list()
+    std = list()
     mu_values = list()
     for result_file in results_dir:
         if graph_name in result_file:
-            with open(results_path+result_file,'r') as file:
-                res = file.read()
-                res = ast.literal_eval(res)
-                list_of_res.append(res)
-                mu_values.append(result_file.split('_')[-1][-3:])
-    plot_res(list_of_res,mu_values)
+            if 'p_values' in result_file:
+                with open(results_path+result_file,'r') as file:
+                    res = file.read()
+                    res = ast.literal_eval(res)
+                    list_of_res.append(res)
+                    mu_values.append(result_file.split('_')[-1][-3:])
+            elif 'std' in result_file:
+                with open(results_path+result_file,'r') as file:
+                    res = file.read()
+                    res = ast.literal_eval(res)
+                    std.append(res)
+
+    plot_res(list_of_res,mu_values,std)
